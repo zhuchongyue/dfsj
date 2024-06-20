ca11a1f00f0e963b4c4f483fae3cacd76f65d522
yangbo <1747837358@qq.com>
2024-06-20 08:59:41 +0800

    feat: (components) 增加时间轴导出

--- packages/components/src/components/NodeAxis/index.ts
+++ packages/components/src/components/NodeAxis/index.ts
@@ -2,11 +2,15 @@ import AbstractNodeProvider from "./src/AbstractNodeProvider";
 import CanvasNodeRender from "./src/CanvasNodeRender";
 import NodeAxis from "./src/NodeAxis.vue";
 import NullLayerProvider from "./src/NullLayerProvider";
+import {Extensible,TimeModes,Direction} from "./src/enum"
 
 
 export {
     CanvasNodeRender,
     AbstractNodeProvider,
     NodeAxis,
-    NullLayerProvider
+    NullLayerProvider,
+    Extensible,
+    TimeModes,
+    Direction
 }
